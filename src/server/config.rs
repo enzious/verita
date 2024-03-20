a2bc4770e96de78cfc0c824c1a488569091c3049
pika <enzi@braindead.io>
2024-03-19 21:23:48 -0500

    chore: cleanup config

--- src/server/config.rs
+++ src/server/config.rs
@@ -4,7 +4,10 @@ use std::path::PathBuf;
 use std::sync::RwLock;
 
 use clap::Parser;
-use fuzion_commons::config::{DatabaseConfig, HttpConfigWithPublic, LoggingConfig};
+use fuzion_commons::config::{
+  clap_arg_to_log_level, DatabaseConfig, HttpConfigWithPublic, LoggingConfig,
+};
+use fuzion_commons::serde::default_false;
 use toml;
 
 const DEFAULT_CONFIG: &'static str = r##"encoding = "utf-8"
@@ -36,35 +39,18 @@ lazy_static! {
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct FuzionVeritaConfig {
+  #[serde(skip)]
   pub raw: Option<toml::Value>,
 
   pub encoding: String,
-  #[serde(default)]
-  pub interactive: bool,
+  #[serde(default = "default_false")]
+  pub non_interactive: bool,
   #[serde(default)]
   pub migrate: bool,
 
   pub logging: LoggingConfig,
   pub database: DatabaseConfig,
   pub http: HttpConfigWithPublic,
-}
-
-impl Default for FuzionVeritaConfig {
-  fn default() -> Self {
-    let config = FuzionVeritaConfig {
-      raw: None,
-
-      encoding: "utf8".to_owned(),
-      interactive: false,
-      migrate: false,
-
-      logging: Default::default(),
-      database: Default::default(),
-      http: Default::default(),
-    };
-
-    config
-  }
 }
 
 #[derive(Parser, Debug)]
@@ -77,38 +63,27 @@ pub struct FuzionVeritaArgs {
   #[arg(long)]
   migrate: Option<bool>,
   /// If the program is run as a daemon or non-interactive process.
-  #[arg(long)]
+  #[arg(short, long, default_value = "false")]
   non_interactive: Option<bool>,
   /// The verbosity of logging.
   #[arg(long, short, value_parser = clap_arg_to_log_level)]
   log_level: Option<slog::Level>,
 }
 
-pub fn clap_arg_to_log_level(level: &str) -> Result<slog::Level, String> {
-  match level {
-    "critical" => Ok(slog::Level::Critical),
-    "debug" => Ok(slog::Level::Debug),
-    "error" => Ok(slog::Level::Error),
-    "trace" => Ok(slog::Level::Trace),
-    "warning" => Ok(slog::Level::Warning),
-    "info" => Ok(slog::Level::Info),
-    _ => Err(String::from("Failed to parse log level.")),
-  }
-}
-
 impl FuzionVeritaConfig {
   pub fn set(server_config: FuzionVeritaConfig) {
     let mut lock = FUZION_VERITA_CONFIG
       .write()
-      .expect("Could not set ServerConfig");
+      .expect("Could not set FuzionVeritaConfig.");
     *lock = Some(server_config);
   }
 
   pub fn get() -> FuzionVeritaConfig {
-    let lock = FUZION_VERITA_CONFIG
+    FUZION_VERITA_CONFIG
       .read()
-      .expect("Could not get ServerConfig");
-    (*lock).as_ref().unwrap().to_owned()
+      .expect("Could not get FuzionVeritaConfig.")
+      .to_owned()
+      .unwrap()
   }
 
   pub fn load() -> FuzionVeritaConfig {
@@ -117,8 +92,8 @@ impl FuzionVeritaConfig {
     let mut config = FuzionVeritaConfig::load_from_file(&args.config);
 
     // Apply command line arguments.
-    config.migrate = args.migrate.unwrap_or(false);
-    config.interactive = !args.non_interactive.unwrap_or(false);
+    config.migrate = args.migrate.unwrap_or(config.migrate);
+    config.non_interactive = !args.non_interactive.unwrap_or(config.non_interactive);
     config.logging.log_level = args.log_level.unwrap_or(config.logging.log_level);
 
     config
